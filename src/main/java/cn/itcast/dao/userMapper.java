7a44319f894bfc0e4c6963851688a47af418d88e
love52saber <1457659650@qq.com>
2018-09-25 17:40:07 +0800

    Initial commit

---
+++ src/main/java/cn/itcast/dao/userMapper.java
@@ -0,0 +1,17 @@
+package cn.itcast.dao;
+
+import cn.itcast.pojo.user;
+
+public interface userMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(user record);
+
+    int insertSelective(user record);
+
+    user selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(user record);
+
+    int updateByPrimaryKey(user record);
+}
